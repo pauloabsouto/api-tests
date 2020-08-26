fce0a290c3a8fa6ded4d97aeafff52b28b2fdfd0
pauloabsouto <pauloabsouto@gmail.com>
2020-08-26 17:04:38 -0300

    Versão inicial do projeto.

---
+++ src/test/java/br/pb/pauloabsouto/tasks/apitest/APITest.java
@@ -0,0 +1,50 @@
+package br.pb.pauloabsouto.tasks.apitest;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.hamcrest.CoreMatchers;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class APITest {
+
+    @BeforeClass
+    public static void setup(){
+        RestAssured.baseURI = "http://localhost:8001/tasks-backend";
+    }
+
+    @Test
+    public void taskResponseTest(){
+        RestAssured.given()
+                .when()
+                    .get("/todo")
+                .then()
+                    .statusCode(200);
+    }
+
+    @Test
+    public void addTaskTest(){
+        RestAssured.given()
+                    .body("{ \"task\": \"API Test\", \"dueDate\": \"2020-12-30\" }")
+                    .contentType(ContentType.JSON)
+                .when()
+                    .post("/todo")
+                .then()
+                    .statusCode(201);
+    }
+
+    @Test
+    public void addInvalidTaskTest(){
+        RestAssured.given()
+                    .body("{ \"task\": \"API Test\", \"dueDate\": \"2013-12-30\" }")
+                    .contentType(ContentType.JSON)
+                .when()
+                    .post("/todo")
+                .then()
+                    .log().all()
+                    .statusCode(400)
+                .body("message", CoreMatchers.is("Due date must not be in past"));
+    }
+
+
+}
